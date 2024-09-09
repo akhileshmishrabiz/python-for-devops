9e0e4047e25aff36aadc7108850e5dd96ec2d9c6
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-09-09 17:22:16 +0530

    Create patch_ASG_instances.py

---
+++ aws-scripts/ssm_ec2_patching/patch_ASG_instances.py
@@ -0,0 +1,59 @@
+import boto3
+
+def get_instance_ids_from_asg(asg_name: str) -> list:
+    """
+    Retrieves a list of EC2 instance IDs that are part of the specified Auto Scaling group.
+    
+    :param asg_name: Name of the Auto Scaling group
+    :return: List of instance IDs in the Auto Scaling group
+    """
+    # Create an Auto Scaling client
+    autoscaling = boto3.client('autoscaling')
+    
+    # Get the details of the specified Auto Scaling group
+    response = autoscaling.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])
+    
+    # Extract instance IDs from the response
+    instance_ids = [
+        instance['InstanceId'] 
+        for instance in response['AutoScalingGroups'][0]['Instances'] 
+        if instance['LifecycleState'] == 'InService'
+    ]
+    
+    return instance_ids
+
+
+def send_ssm_command(instance_ids: list, association_id: str):
+    """
+    Sends the AWS-RunPatchBaseline SSM command to the provided instance IDs.
+    
+    :param instance_ids: List of EC2 instance IDs to send the command to
+    """
+    if not instance_ids:
+        print("No running instances found to patch.")
+        return
+
+    response = ssm.send_command(
+        DocumentName="AWS-RunPatchBaseline",
+        DocumentVersion="1",
+        Targets=[{'Key': 'InstanceIds', 'Values': instance_ids}],
+        Parameters={
+            "AssociationId": [association_id],
+            "Operation": ["Install"],
+            "RebootOption": ["RebootIfNeeded"],
+            "SnapshotId": [""]
+        },
+        TimeoutSeconds=600,
+        MaxConcurrency="50",
+        MaxErrors="0",
+    )
+    
+    print(f"Command sent. Command ID: {response['Command']['CommandId']}")
+
+# Example usage
+asg_name = "cluster-training"
+instance_ids = get_instance_ids_from_asg(asg_name)
+
+association_id = "dd668724-35c7-4313-bd65-eeac529a8e23"
+send_ssm_command(instance_ids, )
+
